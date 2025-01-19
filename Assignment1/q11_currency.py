29b63cfbc0e727bfd75f144605c0f98c9866e98f
Sonam Sharma <dev.sonam777@gmail.com>
2025-01-19 14:14:23 +0530

    extra code removed

--- Assignment1/q11_currency.py
+++ Assignment1/q11_currency.py
@@ -1,7 +1,4 @@
 total_amount = int(input("Enter the total amount to be withdrawn (in hundreds): "))
-note_100 = 0
-note_50 = 0
-note_10 = 0
 
 note_100 = total_amount // 100
 total_amount = total_amount % 100 
@@ -11,7 +8,6 @@ total_amount = total_amount % 50
 
 note_10 = total_amount // 10
 total_amount = total_amount % 10  
-
 
 print(f"Total 100 notes: {note_100}")
 print(f"Total 50 notes: {note_50}")
